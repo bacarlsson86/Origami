d2f38860bd371c154ea933a27a800ae61a2497fb
Bjorn Carlsson <bacarlsson@gmail.com>
2025-03-28 17:30:52 -0700

    rendering a plane with two different colors

--- threejs-responsive.js
+++ threejs-responsive.js
@@ -14,34 +14,51 @@ function main() {
     const scene = new THREE.Scene()
 
     // Make our box
-    const boxWidth = 1;
-    const boxHeight = 1;
-    const boxDepth = 1;
-    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-    const material = new THREE.MeshPhongMaterial({color: 0x44aa88});
-    const cube = new THREE.Mesh(geometry, material)
+    // const boxWidth = 1;
+    // const boxHeight = 1;
+    // const boxDepth = 1;
+    // const box_geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+
+    // Make a plane
+    const planeWidth = 1
+    const planeHeight = 1
+    const plane_geometry = new THREE.BoxGeometry(planeWidth, planeHeight, 0.001)
+
+    // Define the colors of the "plane"
+    // the last two are the front and back of the "plane"
+    const materials = [
+        new THREE.MeshBasicMaterial({color: 0xFFFFFF}),
+        new THREE.MeshBasicMaterial({color: 0xFF0000}),
+        new THREE.MeshBasicMaterial({color: 0xFFFFFF}),
+        new THREE.MeshBasicMaterial({color: 0xFFFFFF}),
+        new THREE.MeshBasicMaterial({color: 0xFF0000}),
+        new THREE.MeshBasicMaterial({color: 0xFFFFFF}),
+      ];
 
     // Make a light
     const color = 0xFFFFFF;
     const intensity = 3;
     const light = new THREE.DirectionalLight(color, intensity);
     light.position.set(-1, 2, 4);
+    
 
 
 
-    function makeInstance(geometry, color, x){
-        const material = new THREE.MeshPhongMaterial({color})
+    function makeInstance(geometry, color, x, materials){
+        // const material = new THREE.MeshBasicMaterial({color})
 
-        const cube = new THREE.Mesh(geometry, material)
-        cube.position.x = x;
-        return cube
+        const instance = new THREE.Mesh(geometry, materials)
+        instance.position.x = x;
+        return instance
     }
 
-    const cubes = [
-        makeInstance(geometry, 0x44aa88,  0),
-        makeInstance(geometry, 0x8844aa, -2),
-        makeInstance(geometry, 0xaa8844,  2),
-    ];
+    // const cubes = [
+    //     makeInstance(box_geometry, 0x44aa88,  0),
+    //     makeInstance(box_geometry, 0x8844aa, -2),
+    //     makeInstance(box_geometry, 0xaa8844,  2),
+    // ];
+
+    const plane = makeInstance(plane_geometry, 0x8844aa, 0, materials)
 
     function resizeRendererToDisplaySize(renderer){
         const canvas = renderer.domElement;
@@ -64,14 +81,18 @@ function main() {
         camera.updateProjectionMatrix();
         }
 
-        cubes.forEach((cube, ndx) => {
-        const speed = 1 + ndx * .1;
-        const rot = time * speed
-        cube.rotation.x = rot;
-        cube.rotation.y = rot;
-        })
+        // cubes.forEach((cube, ndx) => {
+        // const speed = 1 + ndx * .1;
+        // const rot = time * speed
+        // cube.rotation.x = rot;
+        // cube.rotation.y = rot;
+        // })
 
-        scene.add(...cubes, light);
+        plane.rotation.x = time * .5
+        plane.rotation.z = time * .5
+
+        // scene.add(...cubes, light);
+        scene.add(plane, light)
         renderer.render(scene, camera)
 
         requestAnimationFrame(render)
